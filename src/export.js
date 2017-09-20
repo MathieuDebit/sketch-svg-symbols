57a763f5f68d57ef57525024011d67568895f9a7
Mathieu Débit <mathieu.debit@gmail.com>
2017-09-20 03:22:06 +0200

    Read & Write SVG files

--- src/export.js
+++ src/export.js
@@ -1,4 +1,9 @@
+import fs from 'sketch-module-fs'
+
+const TMP_FOLDER = '/tmp/sketch-svg-symbols';
+
 export default function (context) {
+  // Export Artboards
   const sketch = context.api();
 
   const document = sketch.selectedDocument;
@@ -7,10 +12,34 @@ export default function (context) {
   const options = {
     "scales": "1",
     "formats": "svg",
-    "output": "~/Projects/sketch-svg-symbols/tmp"
+    "output": TMP_FOLDER
   };
 
   page.exportArtboards(options);
 
-  context.document.showMessage('Exported 🙌');
+
+  // Create final SVG file
+  const SVGs = NSFileManager.defaultManager().subpathsAtPath(TMP_FOLDER);
+  let exportFile;
+
+  for (var i = 0; i < SVGs.length; i++) {
+    const svg = SVGs[i];
+    const file = fs.readFile(`${TMP_FOLDER}/${svg}`);
+
+    exportFile += file;
+
+    console.log(svg);
+    console.log(file);
+    console.log('________________');
+  }
+
+  fs.writeFile(`${TMP_FOLDER}/export.svg`, exportFile);
+  console.log('_________________');
+  console.log('EXPORT FILE');
+  console.log('_________________');
+  console.log(fs.readFile(`${TMP_FOLDER}/export.svg`));
+
+
+  // Done
+  context.document.showMessage('Done 🙌');
 };
